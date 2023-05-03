8f8fc14cbd12a1828ca164ec4cdcc8dab5b50cf4
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-05-03 01:00:13 -0700

    fix and don't take hugo version under v.0.84.0

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Installation of Go-Hugo and Make applications
-apt-get update && apt-get install -y hugo make wget
+# Update package lists and installation of Make applications
+apt-get update && apt-get install -y make wget
 
 # getting the latest release of 'hugo'
 HUGO_VERSION=0.84.0
