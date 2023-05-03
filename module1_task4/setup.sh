5e7d51768f9b7231d06044e5d55af85fe51e62b7
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-05-03 00:05:23 -0700

    remove the docker command

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# spawn a sandboxed environment in an Ubuntu 18.04 Docker
-docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
-
 # Installation of Go-Hugo and Make applications
 apt-get update && apt-get install -y hugo make
 
