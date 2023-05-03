f9719abd77520c55eb2e55fc6e4df67f60d17c99
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-05-03 00:49:31 -0700

    don't take hugo version under v.0.84.0

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -4,12 +4,13 @@
 apt-get update && apt-get install -y hugo make wget
 
 # getting the latest release of 'hugo'
+HUGO_VERSION=0.84.0
 wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.d
 
 # install hugo
 dpkg -i hugo_${HUGO_VERSION}_Linux-64bit.deb
 
-# rm hugo_${HUGO_VERSION}_Linux-64bit.deb
+rm hugo_${HUGO_VERSION}_Linux-64bit.deb
 
 # Running the command `make build` to build the website
 make build
