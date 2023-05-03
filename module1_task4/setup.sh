1a4fe07647e382039d6cbf2f052c783f1a511b33
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-05-03 00:24:23 -0700

    fix command to install hugo in the latest version

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -9,7 +9,7 @@ wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${
 # install hugo
 dpkg -i hugo_${HUGO_VERSION}_Linux-64bit.deb
 
-rm hugo_${HUGO_VERSION}_Linux-64bit.deb
+# rm hugo_${HUGO_VERSION}_Linux-64bit.deb
 
 # Running the command `make build` to build the website
 make build
