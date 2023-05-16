062296278dfcc86fea6bcf0509d5eddae2cbeed5
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-05-16 08:07:50 -0700

    change setup file and workflow

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# Update package lists and installation of required packages
-sudo apt-get update && sudo apt-get install -y make git
+# Install required packages
+apt-get update && apt-get install -y curl make
 
-# download minimal version to use 'hugo' with the template ananke
-sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
+# Install Hugo
+curl -LO https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
+tar -xzf hugo_extended_0.84.0_Linux-64bit.tar.gz -C /usr/local/bin/
+rm hugo_extended_0.84.0_Linux-64bit.tar.gz
+export PATH=/usr/local/bin:$PATH
 
-# install hugo
-sudo apt install ./hugo.deb
-
-# remove file after the installation
-sudo rm hugo.deb
-
-# download and install linter
-sudo go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.52.2
-sudo npm install -g markdownlint-cli
-sudo npm install -g markdown-link-check
+# Run make build
+make build
