25e3915f8a74bd0c02c9ad3275eefe018e8b9540
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-05-21 04:48:33 -0700

    remove check and add docker run

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update package lists and installation of required packages
-apt-get update && apt-get install -y hugo make
+apt-get update && apt-get install -y hugo make npm
 
 # download minimal version to use 'hugo' with the template ananke
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
@@ -15,5 +15,6 @@ rm hugo.deb
 # Running the command `make build` to build the website
 make build
 
-# Remove go
-rm -rf /usr/local/go
+# Install markdownlint-cli and markdown-link-check
+sudo npm install -g markdownlint-cli
+sudo npm install -g markdown-link-check
